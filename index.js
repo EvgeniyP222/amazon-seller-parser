1e6d6403a9590c5ccd6a68d27e4b159bf4875e8c
EvgeniyP222 <work222all@gmail.com>
2025-04-28 12:07:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,9 +40,8 @@ app.listen(PORT, () => {
 app.get('/parse', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-    executablePath: '/opt/render/project/.render/chrome/opt/google/chrome/chrome',
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  headless: true,
 });
     const page = await browser.newPage();
     
