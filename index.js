e45f37b426e239c7ddcb2f5212a32890191dcc87
EvgeniyP222 <work222all@gmail.com>
2025-04-28 13:06:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('playwright-chromium'); // Используем puppeteer-core
+const { chromium } = require('playwright-chromium'); // Используем puppeteer-core
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 app.get('/parse', async (req, res) => {
   let browser;
   try {
-    browser = await puppeteer.launch({
+    const browser = await chromium.launch({ ... });
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
       headless: true,
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser'
